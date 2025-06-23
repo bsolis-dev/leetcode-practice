526ca0d7500a4d2c57ca58fd568b737cf98253e7
Bryan Solis <bryanmatthewsolis@gmail.com>
2025-06-22 21:42:48 -0700

    Updated approach to Merge String Alternately

--- Strings/merge_alternately.cpp
+++ Strings/merge_alternately.cpp
@@ -3,7 +3,7 @@ Problem: https://leetcode.com/problems/merge-strings-alternately
 
 Description: Given two strings word1 and word2, merge them by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
-Approach: iterate max(m, n) times, appending the ith character of word1 and word2 (if any left)
+Approach: Iterate max(m, n) times, appending the ith character of word1 and word2 (if any left)
 
 Time Complexity: O(m + n)
 Space Complexit: O(1)
